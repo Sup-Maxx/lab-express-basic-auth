28a749f7f03f1c670e42c947c8edb7ddb7acea58
Maksym Kopychanskyi <kopy.chan.max@gmail.com>
2023-01-12 22:28:45 +0000

    added user.Create()

--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -1,16 +1,31 @@
 const router = require("express").Router()
+const bcrypt = require("bcryptjs")
+saltRounds = 10
 
+const User = require("../models/user.model")
 
 router.get("/signup", (req,res) => {
     res.render("auth/signup")
 })
 
 router.post("/signup", (req,res) => {
-    console.log(req.body)
     const {username, password} = req.body
 
-    .then((user) => console.log(user))
+    bcrypt
+    .genSalt(saltRounds)
+    .then((salt) => {
+        console.log("password: " + password)
+        console.log("salt: " + salt)
+        return bcrypt.hash(password, salt)
+    })
+    .then((hashedPassword) => {
+        console.log("New account was created. Username: " + username + " with password " + hashedPassword)
+        User.create({
+            username: username,
+            passwordHash: hashedPassword
+    })
     res.redirect("/profile")
+})
 })
 
 router.get("/profile", (req,res) => {
