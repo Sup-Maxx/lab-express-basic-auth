50f12881a5d47657313e49867813be43b35bd98d
Maksym Kopychanskyi <kopy.chan.max@gmail.com>
2023-01-12 20:26:02 +0000

    routes created

--- models/User.model.js
+++ models/User.model.js
@@ -2,12 +2,23 @@ const { Schema, model } = require("mongoose");
 
 // TODO: Please make sure you edit the user model to whatever makes sense in this case
 const userSchema = new Schema({
-  username: {
-    type: String,
-    unique: true
+  
+    username: {
+      type: String,
+      required: [true, 'Username is required.'],
+      unique: true,
+      lowercase: true,
+      trim: true
+    },
+    passwordHash: {
+      type: String,
+      required: [true, 'Password is required.']
+    }
   },
-  password: String
-});
+  {
+    timestamps: true
+  }
+);
 
 const User = model("User", userSchema);
 
---
+++ routes/auth.routes.js
@@ -0,0 +1,18 @@
+const router = require("express").Router()
+
+
+router.get("/signup", (req,res) => {
+    res.render("auth/signup")
+})
+
+router.post("/signup", (req,res) => {
+    console.log(req.body)
+    const {username, password} = req.body
+    res.redirect("/profile")
+})
+
+router.get("/profile", (req,res) => {
+    res.render("user/user-profile")
+})
+
+module.exports = router
